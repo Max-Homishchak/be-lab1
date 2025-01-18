bfb5601cb0f3c25d8a291e9462b710e913ab7584
Maksym Khomishchak <mkhomishchak@amelco.co.uk>
2025-01-18 10:49:01 +0000

    initial project setup

---
+++ src/main/java/com/khomishchak/belab1org/BeLab1OrgApplication.java
@@ -0,0 +1,13 @@
+package com.khomishchak.belab1org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLab1OrgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeLab1OrgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khomishchak/belab1org/BeLab1OrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khomishchak.belab1org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeLab1OrgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
